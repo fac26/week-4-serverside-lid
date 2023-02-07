d4f3bf1c2ade3b8dd3f25be63c27da6e8b387768
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-02-07 11:48:29 +0000

    Work on seeding

--- src/database/seed.sql
+++ src/database/seed.sql
@@ -1,34 +1,31 @@
 PRAGMA foreign_keys = OFF;
+
 BEGIN;
 
-INSERT INTO authors VALUES
-(1, 'J.R.R Tolkien'),
-(2, 'Stephen King'),
-(3, 'Arthur C. Clarke'),
-(4, 'George Orwell'),
-(5, 'Unknown')
+INSERT INTO films VALUES
+(1, 'Star Wars', 1977, 'George Lucas', 1),
+(2, 'The Exorcist', 1973, 'William Friedkin', 2),
+(3, 'Schindlers List', 1993, 'Steven Spielberg', 3),
+(4, 'The Lord of the Rings: The Return of the King', 2003, 'Peter Jackson', 4),
+(5, 'JFK', 1991, 'Oliver Stone', 5)
 
 ON CONFLICT DO NOTHING;
 
 INSERT INTO genres VALUES
-(1, 'Fantasy'),
+(1, 'Science Fiction'),
 (2, 'Horror'),
-(3, 'Science Fiction'),
-(4, 'Drama'),
+(3, 'Drama'),
+(4, 'Fantasy'),
 (5, 'Politics')
 
 ON CONFLICT DO NOTHING;
 
-INSERT INTO books (name, author_id, year, genres_id) VALUES
-  ('The Lord of the Rings', 1, 1955, 1),
-  ('The Hobbit', 1,  1937, 1),
-  ('It', 2, 1986, 2),
-  ('The Stand', 2, 1978, 2),
-  ('2001: A Space Oddysey', 3, 1968, 3),
-  ('Rendezvous With Rama', 3, 1973, 3),
-  ('1984', 4, 1948, 4),
-  ('Homage to Catalonia', 4, 1938, 5)
+INSERT INTO photo () VALUES
 
 ON CONFLICT DO NOTHING;
+
+
+
 COMMIT;
+
 PRAGMA foreign_keys = ON;
