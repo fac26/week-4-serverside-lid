dedf82c17e652a4400ae0c311d7587d7e6309064
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-02-07 11:58:44 +0000

    Seeding seeds

--- src/database/seed.sql
+++ src/database/seed.sql
@@ -2,16 +2,16 @@ PRAGMA foreign_keys = OFF;
 
 BEGIN;
 
-INSERT INTO films VALUES
+INSERT INTO films (id, name, year, director, genre_id) VALUES
 (1, 'Star Wars', 1977, 'George Lucas', 1),
-(2, 'The Exorcist', 1973, 'William Friedkin', 2),
+(2, 'Jaws', 1973, 'Steven Spielberg', 2),
 (3, 'Schindlers List', 1993, 'Steven Spielberg', 3),
 (4, 'The Lord of the Rings: The Return of the King', 2003, 'Peter Jackson', 4),
 (5, 'JFK', 1991, 'Oliver Stone', 5)
 
 ON CONFLICT DO NOTHING;
 
-INSERT INTO genres VALUES
+INSERT INTO genres (id, genre_name) VALUES
 (1, 'Science Fiction'),
 (2, 'Horror'),
 (3, 'Drama'),
@@ -20,12 +20,24 @@ INSERT INTO genres VALUES
 
 ON CONFLICT DO NOTHING;
 
-INSERT INTO photo () VALUES
+INSERT INTO photo (id_ photo, film_id, user_id) VALUES
+(1, photo, $, $)
+(2, photo, $, $)
 
 ON CONFLICT DO NOTHING;
 
+INSERT INTO users (id, email, hash, created_at) VALUES
+(1, 'a@example.com', '$2a$12$A74tKw96m82AEZpJrIEQxecZGscKayJD/hD5/I6DuqKEJoQlAlNYO', '2023-02-07 00:00:00'),
+(2, 'b@example.com', '$2a$12$o2NP8ykiki.T11qRQOCnBeX68PhwXHprM/hxHkbAfqotWo2XT.vdW', '2017-12-25 00:00:00')
+
+ON CONFLICT DO NOTHING;
+
+INSERT INTO sessions (id, user_id, expires_at, created_at) VALUES
+(1, 1, '2023-02-15 00:00:00', '2023-02-07 00:00:00'),
 
 
 COMMIT;
 
 PRAGMA foreign_keys = ON;
+
+INSERT INTO tables ()
