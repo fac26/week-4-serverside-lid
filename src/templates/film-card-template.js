644610f7163226a1504281b752eded04f03083f4
LauraK0 <laurakeating@live.co.uk>
2023-02-08 11:25:29 +0000

    fixerrors

---
+++ src/templates/film-card-template.js
@@ -0,0 +1,37 @@
+const { checkCurrentUser } = require("../routes/current-user");
+
+function filmCardsTemplate(session, filmsFromDB) {
+  const filmsUL = /*html*/ `
+    <ul class="films-list">${filmsFromDB
+      .map((film) => filmCardTemplate(film, session))
+      .join("")}</ul>
+    `;
+  return filmsUL;
+}
+
+function filmCardTemplate(film, session = {}) {
+  const filmTemplate = /*html*/ `
+        <li class="film">
+        <h4>${film.name}</h4>
+        <p>${film.year}</p>   
+    `;
+  if (checkCurrentUser(film, session) === false) {
+    return /*html*/ `
+        ${filmTemplate}
+    </li>
+        `;
+  }
+  return /*html*/ `
+        ${filmTemplate}
+        ${deleteButton(film)}`;
+}
+
+function deleteButton(film) {
+  return /*html*/ `
+    <form method="POST" action="/delete-film/${film.id}">
+    <button class="Button" type="submit">Delete</button>
+    </form>
+    `;
+}
+
+module.exports = { filmCardsTemplate };
