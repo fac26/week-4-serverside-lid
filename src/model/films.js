3cb5a7b02d0477cd66caa01e0f1e777644ac63e7
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-02-08 11:44:26 +0000

    Updates to file

--- src/model/films.js
+++ src/model/films.js
@@ -1,18 +1,18 @@
 //adding film data including pictures
-//then creating a filmname.js file to filter through the films 
+//then creating a filmname.js file to filter through the films
 
-const db = require('../database/db');
+const db = require("../database/db");
 
 //add film to db-user enters on the 'POST secret' page their film (including pic, director, year etc)
 //the films table also accepts the user_id (to know the user signed in can add their film)
 
-const add_film = db.prepare(`
-    INSERT INTO films (name, year, director, genre_id)
-    VALUES (?, ?, ?, ?);
+// const add_film = db.prepare(`
+//     INSERT INTO films (name, year, director, genre_id)
+//     VALUES (?, ?, ?, ?);
 
-    INSERT INTO photos (photo, film_id, user_id)
-    VALUES (?, (SELECT last_insert_rowid() FROM film), ?);
-`);
+//     INSERT INTO photos (photo, film_id, user_id)
+//     VALUES (?, (SELECT last_insert_rowid() FROM film), ?);
+// `);
 
 //so when the user fills in the add film form:
 //name, year, director and genre_id (from drop down box) are added into the film table from user input
@@ -23,9 +23,9 @@ const add_film = db.prepare(`
 //a new row is created in the film table that shows: [5, 'hunger games', 2012, Gary Ross, Science Fiction]
 //then two new rows is created in the photos table that shows [1, 'katniss.png', 5, 1], [2, 'peta.png', 5, 1] (first user so user_id is 1)
 
-function addFilm(name, year, director, genre_id, photo, user_id) {
-    return add_film.run({ name, year, director, genre_id, photo, user_id});
-}
+// function addFilm(name, year, director, genre_id, photo, user_id) {
+//   return add_film.run({ name, year, director, genre_id, photo, user_id });
+// }
 //you're using run instead of get because you arent returning any rows, get makes sense for displaying not inputting
 
 const view_all_films = db.prepare(`
@@ -34,16 +34,14 @@ const view_all_films = db.prepare(`
     films.name,
     films.year,
     films.director,
-    films.genre_id - not sure about this one,
-    photos.photo
+    films.genre_id
     FROM films
-    JOIN photos WHERE photos.film_id = films.id
 `);
 
 function listFilms() {
-    return view_all_films.all();
+  return view_all_films.all();
 }
 
-console.log(listFilms())
+console.log(listFilms());
 
-module.exports = { addFilm, listFilms }
+module.exports = { listFilms };
