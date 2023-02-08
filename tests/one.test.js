06a49c698ca68a5ea7f637286022964c836fb5a7
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-02-08 14:27:14 +0000

    Tests initial commit

---
+++ tests/one.test.js
@@ -0,0 +1,30 @@
+const test = require("node:test");
+const assert = require("node:assert");
+const { sid } = require("../src/routes/sign-up");
+
+const { createUser, createSession, getSession, reset } = require("./helper.js");
+
+test("createSession can create a new account", async () => {
+  // clears the db
+  reset();
+  // create a user
+  const user = createUser("Test", "abc");
+  // create a session id
+  const sid = createSession(user.id);
+  assert.ok(
+    sid,
+    `Expected: createSession to return sid, and received "${sid}".`
+  );
+
+  assert.ok(
+    sid.length > 12,
+    `Expected: sid to be more than 12 characters, and received sid of ${sid.length} characters.`
+  );
+
+  const session = getSession(sid);
+  assert.equal(
+    session.user_id,
+    user.id,
+    `Expected: session stores the user's id, and received session.user_id of: ${session.user_id}`
+  );
+});
