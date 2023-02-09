d19de0cb34d6964382820e5b39cc778d4f2bef22
LauraK0 <laurakeating@live.co.uk>
2023-02-09 11:17:25 +0000

    model/films.js resolved

--- src/model/films.js
+++ src/model/films.js
@@ -1,37 +1,5 @@
-const db = require("../database/db");
+const db = require('../database/db');
 
-<<<<<<< HEAD
-//add film to db-user enters on the 'POST secret' page their film (including pic, director, year etc)
-//the films table also accepts the user_id (to know the user signed in can add their film)
-
-const add_film = db.prepare(`
-    INSERT INTO films (name, year, director, genre_id, user_id)
-    VALUES ($name, $year, $director, $genre_id, $user_id);
-`);
-
-//so when the user fills in the add film form:
-//name, year, director and genre_id (from drop down box) are added into the film table from user input
-//that will automatically generate a new id from the film table because a new row is added (from autoincrement)
-//and then the photos table will create a new row of the new image added along with the last inserted row id
-//which is the most recent/last added row id of the film added in the films table
-//so if i added my 5th film called 'hunger games', and added two pictures:
-//a new row is created in the film table that shows: [5, 'hunger games', 2012, Gary Ross, Science Fiction]
-//then two new rows is created in the photos table that shows [1, 'katniss.png', 5, 1], [2, 'peta.png', 5, 1] (first user so user_id is 1)
-
-function addFilm(name, year, director, genre_id, user_id) {
-  return add_film.run({ name, year, director, genre_id, user_id });
-}
-//you're using run instead of get because you arent returning any rows, get makes sense for displaying not inputting
-
-const view_all_films = db.prepare(`
-    SELECT
-    films.id,
-    films.name,
-    films.year,
-    films.director,
-    films.genre_id,
-    films.user_id
-=======
 const view_all_films = db.prepare(`
     SELECT
     id, 
@@ -41,7 +9,6 @@ const view_all_films = db.prepare(`
     genre_id,
     image_path,
     user_id
->>>>>>> main
     FROM films
 `);
 
@@ -49,19 +16,6 @@ function listAllFilms() {
     return view_all_films.all();
 }
 
-<<<<<<< HEAD
-//delete film from db
-const delete_film = db.prepare(`
-    DELETE FROM films
-    WHERE id = ?
-`);
-
-function deleteFilm(filmid) {
-  delete_film.run(filmid);
-}
-
-module.exports = { addFilm, listFilms, deleteFilm };
-=======
 const get_films = db.prepare(/*sql*/ `
    SELECT 
      id,
@@ -76,7 +30,16 @@ const get_films = db.prepare(/*sql*/ `
 `);
 
 function getFilmsbyUser(user_id) {
-  return get_films.all(user_id);
+    return get_films.all(user_id);
+}
+
+const delete_film = db.prepare(`
+    DELETE FROM films
+    WHERE id = ?
+`);
+
+function deleteFilm(filmid) {
+    delete_film.run(filmid);
 }
 
 const add_film = db.prepare(/*sql*/ `
@@ -85,10 +48,14 @@ const add_film = db.prepare(/*sql*/ `
   `);
 
 function addFilm(name, year, director, genre_id, image_path, user_id) {
-  return add_film.run({name, year, director, genre_id, image_path, user_id})
+    return add_film.run({
+        name,
+        year,
+        director,
+        genre_id,
+        image_path,
+        user_id,
+    });
 }
 
-
-module.exports = { listAllFilms, getFilmsbyUser, addFilm };
-
->>>>>>> main
+module.exports = { listAllFilms, getFilmsbyUser, addFilm, deleteFilm };
