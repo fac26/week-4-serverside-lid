62e5bf241a0c15f8c55ffcb70bc2ab4a97c8cd6c
LauraK0 <laurakeating@live.co.uk>
2023-02-09 10:02:18 +0000

    some files readded after deleting

--- src/templates/film-card-template.js
+++ src/templates/film-card-template.js
@@ -1,37 +1,37 @@
-const { checkCurrentUser } = require("../routes/current-user");
+const { checkCurrentUser } = require('../routes/current-user');
 
-function filmCardsTemplate(session, filmsFromDB) {
-  const filmsUL = /*html*/ `
+function renderCards(session, filmsFromDB) {
+    const filmsUL = /*html*/ `
     <ul class="films-list">${filmsFromDB
-      .map((film) => filmCardTemplate(film, session))
-      .join("")}</ul>
+        .map((film) => filmCardTemplate(film, session))
+        .join('')}</ul>
     `;
-  return filmsUL;
+    return filmsUL;
 }
 
 function filmCardTemplate(film, session = {}) {
-  const filmTemplate = /*html*/ `
+    const filmTemplate = /*html*/ `
         <li class="film">
         <h4>${film.name}</h4>
         <p>${film.year}</p>   
     `;
-  if (checkCurrentUser(film, session) === false) {
-    return /*html*/ `
+    if (checkCurrentUser(film, session) === false) {
+        return /*html*/ `
         ${filmTemplate}
     </li>
         `;
-  }
-  return /*html*/ `
+    }
+    return /*html*/ `
         ${filmTemplate}
         ${deleteButton(film)}`;
 }
 
 function deleteButton(film) {
-  return /*html*/ `
+    return /*html*/ `
     <form method="POST" action="/delete-film/${film.id}">
     <button class="Button" type="submit">Delete</button>
     </form>
     `;
 }
 
-module.exports = { filmCardsTemplate };
+module.exports = { renderCards };
